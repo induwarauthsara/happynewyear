3f44cc37771cdb2733667cab6b726fd10d9beb57
Induwara Uthsara <induwarauthsara@gmail.com>
2020-04-13 23:09:11 +0530

    Add files via upload

---
+++ hny.py
@@ -0,0 +1,16 @@
+
+name = input("Enter Your Name : ")
+x = "Wish You a Happy New Year, "+name
+y = len(x)
+for i in range(y):
+    print(x[0:i+1])
+
+# Happy New Year For All..
+# I am Induwara Uthsara
+# This Is my first python work.
+# Thanks For Watch.
+
+# Contact Me :
+    # Facebook : https://www.fb.com/induwa.uthsara
+    # Whatsapp : 0763547175
+    # Github : https://github.com/induwarauthsara/
